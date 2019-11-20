f77879bc1a8fc64f2c0f6531397643114f54448c
HaedHutner <mvvasilev@outlook.com>
2019-11-20 16:38:13 +0200

    Add description to Slash

--- src/main/java/com/atherys/rpgskills/t1/Slash.java
+++ src/main/java/com/atherys/rpgskills/t1/Slash.java
@@ -1,5 +1,6 @@
 package com.atherys.rpgskills.t1;
 
+import com.atherys.rpg.api.skill.DescriptionArguments;
 import com.atherys.rpg.api.skill.SkillSpec;
 import com.atherys.rpg.api.skill.TargetedRPGSkill;
 import com.atherys.rpgskills.util.CommonProperties;
@@ -9,6 +10,8 @@ import com.google.common.collect.ImmutableMap;
 import org.spongepowered.api.entity.living.Living;
 import org.spongepowered.api.event.cause.entity.damage.DamageTypes;
 import org.spongepowered.api.event.cause.entity.damage.source.EntityDamageSource;
+import org.spongepowered.api.text.TextTemplate;
+import org.spongepowered.api.util.Tuple;
 
 public class Slash extends TargetedRPGSkill {
     private static final String DEFAULT_DAMAGE_EXPRESSION = "CLAMP(SOURCE_STRENGTH * 1.5, 0.5, 10.0)";
@@ -21,6 +24,10 @@ public class Slash extends TargetedRPGSkill {
                         .cooldown("0")
                         .descriptionTemplate("Strikes the target with a powerful blow.")
                         .resourceCost("0")
+                        .descriptionTemplate(TextTemplate.of("Slash at your target, dealing ", TextTemplate.arg("damage"), " physical damage."))
+                        .descriptionArguments(
+                                Tuple.of("damage", DescriptionArguments.ofSource(DEFAULT_DAMAGE_EXPRESSION))
+                        )
                         .properties(ImmutableMap.of(MAX_RANGE_PROPERTY, "5.0"))
         );
     }
