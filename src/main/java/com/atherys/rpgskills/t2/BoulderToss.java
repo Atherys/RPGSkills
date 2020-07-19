d1474f85912c1f34499b2f8c74d1b50fc2ab4cc2
CodeStripper <brentgerstenschlager@gmail.com>
2020-07-19 15:13:57 -0400

    BoulderToss now does a configurable amount of AOE damage on hit, added effects to impact.

--- src/main/java/com/atherys/rpgskills/t2/BoulderToss.java
+++ src/main/java/com/atherys/rpgskills/t2/BoulderToss.java
@@ -1,7 +1,7 @@
 package com.atherys.rpgskills.t2;
 
+import com.atherys.core.utils.Sound;
 import com.atherys.rpg.AtherysRPG;
-import com.atherys.rpg.api.skill.DescriptionArguments;
 import com.atherys.rpg.api.skill.RPGSkill;
 import com.atherys.rpg.api.skill.SkillSpec;
 import com.atherys.rpgskills.util.DamageUtils;
@@ -14,6 +14,11 @@ import com.flowpowered.math.vector.Vector3d;
 import org.spongepowered.api.block.BlockState;
 import org.spongepowered.api.block.BlockTypes;
 import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.effect.particle.ParticleEffect;
+import org.spongepowered.api.effect.particle.ParticleOptions;
+import org.spongepowered.api.effect.particle.ParticleTypes;
+import org.spongepowered.api.effect.sound.SoundTypes;
+import org.spongepowered.api.entity.Entity;
 import org.spongepowered.api.entity.EntityTypes;
 import org.spongepowered.api.entity.FallingBlock;
 import org.spongepowered.api.entity.living.Living;
@@ -21,8 +26,12 @@ import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.block.CollideBlockEvent;
 import org.spongepowered.api.event.entity.CollideEntityEvent;
 import org.spongepowered.api.event.filter.Getter;
+import org.spongepowered.api.util.Color;
 import org.spongepowered.api.util.Tuple;
+import org.spongepowered.api.world.Location;
+import org.spongepowered.api.world.World;
 
+import java.util.Collection;
 import java.util.Map;
 import java.util.UUID;
 import java.util.WeakHashMap;
@@ -34,10 +43,25 @@ import static com.atherys.rpgskills.util.DescriptionUtils.otherText;
 import static org.spongepowered.api.text.TextTemplate.arg;
 
 public class BoulderToss extends RPGSkill implements PartySkill {
+    private static final String AOE_RANGE = "aoe-range";
+    private static final String AOE_DAMAGE_MODIFIER = "aoe-damage-modifier";
+
     private static final String DEFAULT_DAMAGE_EXPRESSION = "5.0";
+    private static final String DEFAULT_AOE_RANGE = "3";
+    private static final String DEFAULT_AOE_DAMAGE_MODIFIER = "3";
     private static final String DEFAULT_OTHER_TEXT = "";
 
-    private Map<UUID, Living> boulders = new WeakHashMap<>();
+    private static final Sound boulder = Sound.builder(SoundTypes.ENTITY_GENERIC_EXPLODE, 3)
+            .pitch(1.2)
+            .build();
+
+    private static final ParticleEffect effect = ParticleEffect.builder()
+            .type(ParticleTypes.LARGE_SMOKE)
+            .option(ParticleOptions.COLOR, Color.GRAY)
+            .quantity(5)
+            .build();
+
+    private final Map<UUID, Living> boulders = new WeakHashMap<>();
 
     public BoulderToss() {
         super(
@@ -56,6 +80,23 @@ public class BoulderToss extends RPGSkill implements PartySkill {
                 Tuple.of(DAMAGE, ofProperty(this, DAMAGE, DEFAULT_DAMAGE_EXPRESSION)),
                 Tuple.of(OTHER_TEXT, otherText(this))
         );
+    }
+
+    private void applyAOEDamage(Location<World> eventLocation, Living user) {
+        double damageModifier = asDouble(user, getProperty(AOE_DAMAGE_MODIFIER, String.class, DEFAULT_AOE_DAMAGE_MODIFIER));
+        double range = asDouble(user, getProperty(AOE_RANGE, String.class, DEFAULT_AOE_RANGE));
+        Collection<Entity> entities = eventLocation.getExtent().getNearbyEntities(eventLocation.getPosition(), range);
+
+        Sound.playSound(boulder, user.getWorld(), eventLocation.getPosition());
+        Vector3d position = eventLocation.getPosition();
+        PhysicsUtils.spawnParticleCloud(effect, eventLocation.setPosition(new Vector3d(position.getX(), position.getFloorY() - 2, position.getZ())));
+
+        entities.forEach(target -> {
+            if (target instanceof Living && !arePlayersInParty(user, (Living) target) && !target.equals(user)) {
+                double damage = (asDouble(user, (Living) target, getProperty(DAMAGE, String.class, DEFAULT_DAMAGE_EXPRESSION))) / damageModifier;
+                target.damage(damage, DamageUtils.directMagical(user));
+            }
+        });
     }
 
     @Override
@@ -84,6 +125,8 @@ public class BoulderToss extends RPGSkill implements PartySkill {
             Living target = (Living) event.getEntities().get(0);
             AtherysRPG.getInstance().getLogger().info("Here");
 
+            applyAOEDamage(target.getLocation(), user);
+
             if (arePlayersInParty(user, target)) return;
 
             if (!target.equals(user)) {
@@ -106,6 +149,8 @@ public class BoulderToss extends RPGSkill implements PartySkill {
     @Listener
     public void onWallCollide(CollideBlockEvent event, @Getter("getSource") FallingBlock boulder) {
         if (boulders.containsKey(boulder.getUniqueId())) {
+            applyAOEDamage(event.getTargetLocation(), boulders.get(boulder.getUniqueId()));
+            boulders.remove(boulder.getUniqueId());
             boulder.remove();
         }
     }
