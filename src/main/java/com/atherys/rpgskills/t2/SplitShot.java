83f877af9265c9f3db527013fb7a8215cf410129
Liam <liamreadsspam@gmail.com>
2020-11-10 16:01:55 -0400

    Do not use data

--- src/main/java/com/atherys/rpgskills/t2/SplitShot.java
+++ src/main/java/com/atherys/rpgskills/t2/SplitShot.java
@@ -7,6 +7,7 @@ import com.atherys.skills.api.exception.CastException;
 import com.atherys.skills.api.skill.CastResult;
 import org.spongepowered.api.data.DataContainer;
 import org.spongepowered.api.entity.EntityTypes;
+import org.spongepowered.api.entity.Transform;
 import org.spongepowered.api.entity.living.Living;
 import org.spongepowered.api.entity.projectile.Projectile;
 import org.spongepowered.api.event.Listener;
@@ -38,23 +39,24 @@ public class SplitShot extends RPGSkill {
         if (AtherysSkills.getInstance().getEffectService().hasEffect(living, SPLITSHOT_EFFECT) && event.getTargetEntity().getType() == EntityTypes.TIPPED_ARROW) {
             AtherysSkills.getInstance().getEffectService().removeEffect(living, SPLITSHOT_EFFECT);
 
-            DataContainer arrowContainer = event.getTargetEntity().toContainer();
+            Projectile originalArrow = event.getTargetEntity();
+            Transform transform = originalArrow.getTransform();
             World world = living.getWorld();
 
-            Projectile arrow1 = (Projectile) world.createEntity(arrowContainer).get();
-            arrow1.setVelocity(arrow1.getVelocity().add(0.5, 0, 0));
+            Projectile arrow1 = (Projectile) world.createEntity(EntityTypes.TIPPED_ARROW, transform.getPosition());
+            arrow1.setVelocity(originalArrow.getVelocity().add(0.5, 0, 0));
             world.spawnEntity(arrow1);
 
-            Projectile arrow2 = (Projectile) world.createEntity(arrowContainer).get();
-            arrow2.setVelocity(arrow1.getVelocity().add(0.25, 0, 0));
+            Projectile arrow2 = (Projectile) world.createEntity(EntityTypes.TIPPED_ARROW, transform.getPosition());
+            arrow2.setVelocity(originalArrow.getVelocity().add(0.25, 0, 0));
             world.spawnEntity(arrow2);
 
-            Projectile arrow3 = (Projectile) world.createEntity(arrowContainer).get();
-            arrow3.setVelocity(arrow1.getVelocity().add(0, 0, 0.5));
+            Projectile arrow3 = (Projectile) world.createEntity(EntityTypes.TIPPED_ARROW, transform.getPosition());
+            arrow3.setVelocity(originalArrow.getVelocity().add(0, 0, 0.5));
             world.spawnEntity(arrow3);
 
-            Projectile arrow4 = (Projectile) world.createEntity(arrowContainer).get();
-            arrow4.setVelocity(arrow1.getVelocity().add(0.5, 0, 0.25));
+            Projectile arrow4 = (Projectile) world.createEntity(EntityTypes.TIPPED_ARROW, transform.getPosition());
+            arrow4.setVelocity(originalArrow.getVelocity().add(0.5, 0, 0.25));
             world.spawnEntity(arrow4);
         }
     }
