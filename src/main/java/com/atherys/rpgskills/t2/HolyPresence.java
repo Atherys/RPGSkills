fd88983552e5fe905554406b917442746eb45114
Aaron Foley <aaron.s.foley@gmail.com>
2020-12-26 23:13:38 +1100

    Add Amplifier and sound FX for Holy-Presence

--- src/main/java/com/atherys/rpgskills/t2/HolyPresence.java
+++ src/main/java/com/atherys/rpgskills/t2/HolyPresence.java
@@ -1,5 +1,6 @@
 package com.atherys.rpgskills.t2;
 
+import com.atherys.core.utils.Sound;
 import com.atherys.rpg.api.skill.DescriptionArguments;
 import com.atherys.rpg.api.skill.RPGSkill;
 import com.atherys.rpg.api.skill.SkillSpec;
@@ -15,14 +16,19 @@ import com.atherys.skills.api.skill.CastResult;
 import com.flowpowered.math.vector.Vector3d;
 import org.spongepowered.api.effect.particle.ParticleEffect;
 import org.spongepowered.api.effect.particle.ParticleTypes;
+import org.spongepowered.api.effect.sound.SoundCategories;
+import org.spongepowered.api.effect.sound.SoundTypes;
 import org.spongepowered.api.entity.Entity;
 import org.spongepowered.api.entity.living.Living;
 import org.spongepowered.api.event.cause.entity.damage.source.DamageSource;
 import org.spongepowered.api.util.Tuple;
+import org.spongepowered.api.world.Location;
+import org.spongepowered.api.world.World;
 
 import java.util.Collection;
 
 import static com.atherys.rpg.api.skill.DescriptionArguments.ofProperty;
+import static com.atherys.rpg.api.skill.DescriptionArguments.ofSlowPercentProperty;
 import static com.atherys.rpgskills.util.CommonProperties.*;
 import static org.spongepowered.api.text.TextTemplate.arg;
 
@@ -30,11 +36,16 @@ public class HolyPresence extends RPGSkill implements PartySkill, RadiusSkill {
 
     private static final String DEFAULT_RADIUS = "10";
     private static final String DEFAULT_DURATION = "4000";
+    private static final String DEFAULT_AMPLIFIER = "0";
 
     private static final ParticleEffect particleEffect = ParticleEffect.builder()
             .type(ParticleTypes.INSTANT_SPELL)
             .quantity(3)
             .build();
+
+    private static final Sound sound = Sound.builder(SoundTypes.ENTITY_EVOCATION_ILLAGER_PREPARE_SUMMON, 1)
+            .pitch(1.5)
+            .soundCategory(SoundCategories.PLAYER).build();
 
     public HolyPresence() {
         super(
@@ -44,13 +55,15 @@ public class HolyPresence extends RPGSkill implements PartySkill, RadiusSkill {
                         .resourceCost("0")
                         .cooldown("0")
                         .descriptionTemplate(DescriptionUtils.buildTemplate(
-                                "You and all enemies within ", arg(RADIUS), " blocks are slowed for ", arg(TIME), " seconds."
+                                "You and all enemies within ", arg(RADIUS), " blocks are slowed by ",
+                                arg(AMPLIFIER), " for ", (TIME), " seconds."
                         ))
         );
 
         setDescriptionArguments(
                 Tuple.of(RADIUS, ofProperty(this, RADIUS, DEFAULT_RADIUS)),
-                Tuple.of(TIME, DescriptionArguments.timeProperty(this, TIME, DEFAULT_DURATION))
+                Tuple.of(TIME, DescriptionArguments.timeProperty(this, TIME, DEFAULT_DURATION)),
+                Tuple.of(AMPLIFIER, ofSlowPercentProperty(this, AMPLIFIER, DEFAULT_AMPLIFIER))
         );
     }
 
@@ -58,14 +71,16 @@ public class HolyPresence extends RPGSkill implements PartySkill, RadiusSkill {
     public CastResult cast(Living user, long timestamp, String... args) throws CastException {
         double radius = asDouble(user, getProperty(RADIUS, String.class, DEFAULT_RADIUS));
         int duration = asInt(user, getProperty(TIME, String.class, DEFAULT_DURATION));
-        int amplifier = asInt(user, getProperty(AMPLIFIER, String.class, "2"));
+        int amplifier = asInt(user, getProperty(AMPLIFIER, String.class, DEFAULT_AMPLIFIER));
+        Location<World> location = user.getLocation();
 
-        applyToRadius(user.getLocation(), radius, living -> {
+        applyToRadius(location, radius, living -> {
             if (!arePlayersInParty(user, living) || living == user) {
                 AtherysSkills.getInstance().getEffectService().applyEffect(living, Effects.ofSlowness("holy-presence", "Holy Presence", duration, amplifier));
             }
         });
 
+        Sound.playSound(sound, location.getExtent(), location.getPosition());
         PhysicsUtils.spawnParticleCircle(particleEffect, user.getLocation(), radius);
 
         return CastResult.success();
