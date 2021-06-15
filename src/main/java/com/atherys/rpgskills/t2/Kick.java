8329c498290d397f075cfa45962e77fd5e44b7c9
cicetil <58564315+cicetil@users.noreply.github.com>
2021-06-14 20:56:47 -0400

    fix maybe

--- src/main/java/com/atherys/rpgskills/t2/Kick.java
+++ src/main/java/com/atherys/rpgskills/t2/Kick.java
@@ -37,7 +37,7 @@ public class Kick extends TargetedRPGSkill {
                         .resourceCost("0")
                         .descriptionTemplate(DescriptionUtils.buildTemplate(
                                 "Deliver a heavy kick to a target enemy, dealing ", arg(DAMAGE), " physical damage and knocking them back.
-                             "  If they collide with an enemy, they both take an additional ", arg(DAMAGE), " physical damage."
+                                If they collide with an enemy, they both take an additional ", arg(DAMAGE), " physical damage."
                         ))
         );
 
